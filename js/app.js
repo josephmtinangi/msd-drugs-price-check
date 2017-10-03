453b51ad07b2674c3cc6862e12bee1812665eecf
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-10-03 12:48:07 +0300

    Add loading spinner

--- js/app.js
+++ js/app.js
@@ -2,16 +2,19 @@ new Vue({
 	el: '#app',
 	data: {
 		query: '',
-		drugs: []
+		drugs: [],
+		isLoading: true,
 	},
 	methods:{
 		search: function() {
+			this.isLoading = false;
 			axios.get('http://htools-tz-msd-drug-tool-api.dokku-1.codefortanzania.org/api/drugs', {
 			    params: {
 			      name: this.query
 			    }
 			  })
 			  .then(function (response) {
+			  	this.isLoading = true;
 			  	this.drugs = response.data.drugs;
 			  }.bind(this))
 			  .catch(function (error) {
