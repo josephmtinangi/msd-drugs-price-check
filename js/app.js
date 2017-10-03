1c4df220b4afeed438382182e198cfba2c28bda6
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-10-03 12:29:01 +0300

    Fetch and render drugs

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 new Vue({
 	el: '#app',
 	data: {
-		query: ''
+		query: '',
+		drugs: []
 	},
 	methods:{
 		search: function() {
-			console.log(this.query);
+			axios.get('http://htools-tz-msd-drug-tool-api.dokku-1.codefortanzania.org/api/drugs', {
+			    params: {
+			      name: 'paracetamol'
+			    }
+			  })
+			  .then(function (response) {
+			  	this.drugs = response.data.drugs;
+			  }.bind(this))
+			  .catch(function (error) {
+			    console.log(error);
+			  });
 		}
 	},
 	mounted() {
